83e3e583962159b20da19885f87d10fab2e590d8
sakurai-akito0219 <k23055kk@aitech.ac.jp>
2024-12-05 15:29:07 +0900

    勝手にファイルいじっちゃったので直しました

--- models/favorite_subject.py
+++
@@ -1,9 +0,0 @@
-from peewee import Model, CharField
-from .db import db
-
-class Favorite_subject(Model):
-    name = CharField()
-    subject = CharField()
-
-    class Meta:
-        database = db
