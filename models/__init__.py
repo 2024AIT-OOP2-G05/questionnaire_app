cd447861bacc79db5b73488efb7554ff2f0bc150
sakurai-akito0219 <k23055kk@aitech.ac.jp>
2024-12-05 15:23:37 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,20 @@ from .db import db
 from .user import User
 from .product import Product
 from .order import Order
+<<<<<<< Updated upstream
+=======
+from.favorite_subject import Favorite_subject
+>>>>>>> Stashed changes
 
 # モデルのリストを定義しておくと、後でまとめて登録しやすくなります
 MODELS = [
     User,
     Product,
     Order,
+<<<<<<< Updated upstream
+=======
+    Favorite_subject
+>>>>>>> Stashed changes
 ]
 
 # データベースの初期化関数
