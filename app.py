d0209d329aad0033d8ea009ff33d92ab1e6b1888
KajitaTakaya <takaya0618.kjt@gmail.com>
2024-12-12 16:04:13 +0900

    ユーザデータをjsonに変更するプログラムの追加
    http://127.0.0.1:8081/users/jsonでjsonページに推移します
    確認お願います

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 from models import initialize_database
 from routes import blueprints
+from models.user import User
 
 
 app = Flask(__name__)
@@ -16,6 +17,20 @@ for blueprint in blueprints:
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/users/json', methods=['GET'])
+def get_users_json():
+    """ユーザーデータをJSON形式で返す"""
+    # データベースから全ユーザー情報を取得
+    users = User.select()
+
+    # データを辞書形式に変換してリスト化
+    users_list = [user.to_dict() for user in users]
+
+    # JSON形式で返す
+    return jsonify(users_list)
+
 
 if __name__ == '__main__':
     app.run(port=8081, debug=True)
